2b67bf8b854ed236cf563177a9822a905809ab25
Thiago Skibinski <thiagoskibinski@gmail.com>
2025-01-16 19:37:26 -0300

    feat: add uuidv7

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,20 +1,34 @@
+import { db } from "@/infra/db";
+import { schema } from "@/infra/db/schemas";
 import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { z } from "zod";
 
 export const uploadImageRoute: FastifyPluginAsyncZod = async (server) => {
-	server.post("/uploads", {
-		schema: {
-			summary: 'Upload an image',
-			body: z.object({
-				name: z.string(),
-				password: z.string().optional(),
-			}),
-			response: {
-				201: z.object({ uploadId: z.string() }),
-				409: z.object({ message: z.string() }).describe('Upload already exists.'),
-			}
-		}
-	}, async (request, reply) => {
-		return reply.status(201).send({ uploadId: 'test'})
-	});
+	server.post(
+		"/uploads",
+		{
+			schema: {
+				summary: "Upload an image",
+				body: z.object({
+					name: z.string(),
+					password: z.string().optional(),
+				}),
+				response: {
+					201: z.object({ uploadId: z.string() }),
+					409: z
+						.object({ message: z.string() })
+						.describe("Upload already exists."),
+				},
+			},
+		},
+		async (request, reply) => {
+			await db.insert(schema.uploads).values({
+				name: "test.jpg",
+				remoteKey: "test.jpg",
+				remoteUrl: "http://test.com",
+			});
+
+			return reply.status(201).send({ uploadId: "test" });
+		},
+	);
 };
