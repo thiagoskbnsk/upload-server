9949ad846b2b1ec1475dfef72482980cbad0fd8f
Thiago Skibinski <thiagoskibinski@gmail.com>
2025-01-16 23:08:13 -0300

    feat: add connection with cloudflare

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,11 @@ const envSchema = z.object({
 	PORT: z.string(),
 	NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
 	DATABASE_URL: z.string().url().startsWith("postgresql://"),
+	CLOUDFLARE_ACCOUNT_ID: z.string(),
+	CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+	CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+	CLOUDFLARE_BUCKET: z.string(),
+	CLOUDFLARE_PUBLIC_URL: z.string().url().startsWith("https://"),
 });
 
 export const env = envSchema.parse(process.env);
